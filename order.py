732286839baab0cd47bb4b81a50095665067d17a
edwinndungu2 <edwin.ndungu2@student.moringaschool.com>
2025-05-29 18:15:00 +0300

    all tests passed

--- order.py
+++ order.py
@@ -1,14 +1,14 @@
 class Order:
     def __init__(self, customer, coffee, price):
         from customer import Customer
-        from Coffee import Coffee
+        from coffee import Coffee
 
         if not isinstance(customer, Customer):
             raise TypeError("customer must be a Customer instance")
         if not isinstance(coffee, Coffee):
             raise TypeError("coffee must be a Coffee instance")
         if not (isinstance(price, float) or isinstance(price, int)):
-            raise TypeError("price must be a float")
+            raise ValueError("price must be a float or int")  # Changed to ValueError
         if not (1.0 <= price <= 10.0):
             raise ValueError("price must be between 1.0 and 10.0")
 
